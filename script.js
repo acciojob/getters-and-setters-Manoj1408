39b9cbb0696c8eb403c294a992be7bca3965c0d0
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-14 15:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Student extends Person {
 		super(name,age);
 	}
 	study(){
-		cosole.log(`${this.name} is studying.`)
+		cosole.log(`${this.name} is studying`)
 	}
 }
 
@@ -26,7 +26,7 @@ class Teacher extends Person {
 		super(name,age);
 	}
 	teach(){
-		console.log(`${this.name} is teaching.`)
+		console.log(`${this.name} is teaching`)
 	}
 
 	
