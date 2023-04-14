895a42304216db78b37a8ff952934617ba4d8a3f
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-14 15:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this._name = name;
-		this._age = age;
+		this.name = name;
+		this.age = age;
 	}
 	get getName(){
-		return this._name
+		return this.name
 	}
 	set setAge(age){
-		this._age = age;
+		this.age = age;
 	}
 }
 
@@ -17,7 +17,7 @@ class Student extends Person {
 		super(name,age);
 	}
 	study(){
-		cosole.log(`${this._name} is studying`)
+		cosole.log(`${this.name} is studying`)
 	}
 }
 
@@ -26,7 +26,7 @@ class Teacher extends Person {
 		super(name,age);
 	}
 	teach(){
-		console.log(`${this._name} is teaching`)
+		console.log(`${this.name} is teaching`)
 	}	
 }
 
